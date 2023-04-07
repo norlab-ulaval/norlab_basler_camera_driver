b088dfa9090e11d7e2607c323a389e3d53c69791
ogamache <o.gamache2@gmail.com>
2023-04-07 07:45:11 -0400

    Updating image decompression node on this branch

--- src/image_decompression_node.cpp
+++ src/image_decompression_node.cpp
@@ -18,8 +18,7 @@ using namespace cv;
 image_transport::Publisher out_image_camera1_pub;
 image_transport::Publisher out_image_camera2_pub;
 
-Mat cv_image1_bayerRG;
-Mat cv_image2_bayerRG;
+
 CImageDecompressor camera1_decompressor;
 CImageDecompressor camera2_decompressor;
 
@@ -31,16 +30,41 @@ cv_bridge::CvImage out_image_msg_2;
 
 void metadata_callback(const norlab_basler_camera_driver::metadata_msg& msg)
 {
+    Mat cv_image1_RGB8_cam1;
+    Mat cv_image2_RGB8_cam2;
+    Mat cv_image1_bayerRG;
+    Mat cv_image2_bayerRG;
     ros::Time time_now = ros::Time::now();
-    camera1_decompressor = CImageDecompressor(msg.descriptor_cam1.data(), msg.descriptor_size_cam1);
-    camera1_decompressor.DecompressImage(camera1_targetImage, msg.imgBuffer_cam1.data(), msg.imgSize_cam1);
+    camera1_decompressor = CImageDecompressor(msg.descriptor_1.data(), msg.size_1);
+    try{
+        camera1_decompressor.DecompressImage(camera1_targetImage, msg.imgBuffer_1.data(), msg.imgSize_1);
+    }
+    catch(...){
+        cout << "lost an image cam1" << endl;
+    }
     out_image_msg_1.header.stamp = time_now;
-    out_image_msg_1.image = Mat(camera1_targetImage.GetHeight(), camera1_targetImage.GetWidth(), CV_8UC1, (uint8_t *) camera1_targetImage.GetBuffer());
+    cv_image1_bayerRG = Mat(camera1_targetImage.GetHeight(), camera1_targetImage.GetWidth(), CV_16UC1, (uint16_t *) camera1_targetImage.GetBuffer());
 
-    camera2_decompressor = CImageDecompressor(msg.descriptor_cam2.data(), msg.descriptor_size_cam2);
-    camera2_decompressor.DecompressImage(camera2_targetImage, msg.imgBuffer_cam2.data(), msg.imgSize_cam2);
+    Mat cv_image1_RGB16(cv_image1_bayerRG.cols, cv_image1_bayerRG.rows, CV_16UC3);
+    cvtColor(cv_image1_bayerRG, cv_image1_RGB16, COLOR_BayerRG2RGB);
+    cv_image1_RGB16.convertTo(cv_image1_RGB8_cam1, CV_8UC3, 1.0/16);
+    out_image_msg_1.image = cv_image1_RGB8_cam1;
+
+
+    camera2_decompressor = CImageDecompressor(msg.descriptor_2.data(), msg.size_2);
+    try{
+        camera2_decompressor.DecompressImage(camera2_targetImage, msg.imgBuffer_2.data(), msg.imgSize_2);
+    }
+    catch(...){
+        cout << "lost an image cam2" << endl;
+    }
     out_image_msg_2.header.stamp = time_now;
-    out_image_msg_2.image = Mat(camera2_targetImage.GetHeight(), camera2_targetImage.GetWidth(), CV_8UC1, (uint8_t *) camera2_targetImage.GetBuffer());
+    cv_image2_bayerRG = Mat(camera2_targetImage.GetHeight(), camera2_targetImage.GetWidth(), CV_16UC1, (uint16_t *) camera2_targetImage.GetBuffer());
+
+    Mat cv_image2_RGB16(cv_image2_bayerRG.cols, cv_image2_bayerRG.rows, CV_16UC3);
+    cvtColor(cv_image2_bayerRG, cv_image2_RGB16, COLOR_BayerRG2RGB);
+    cv_image2_RGB16.convertTo(cv_image2_RGB8_cam2, CV_8UC3, 1.0/16);
+    out_image_msg_2.image = cv_image2_RGB8_cam2;
 
     out_image_camera1_pub.publish(out_image_msg_1.toImageMsg());
     out_image_camera2_pub.publish(out_image_msg_2.toImageMsg());
@@ -57,8 +81,8 @@ int main(int argc, char **argv)
 
     out_image_msg_1.header.frame_id = "Camera1";
     out_image_msg_2.header.frame_id = "Camera2";
-    out_image_msg_1.encoding = sensor_msgs::image_encodings::BAYER_RGGB8;
-    out_image_msg_2.encoding = sensor_msgs::image_encodings::BAYER_RGGB8;
+    out_image_msg_1.encoding = sensor_msgs::image_encodings::TYPE_8UC3;
+    out_image_msg_2.encoding = sensor_msgs::image_encodings::TYPE_8UC3;
     
 
     ros::Subscriber metadata_subscriber = nh.subscribe("/stereo/image_metapackets", 1, metadata_callback);
@@ -67,3 +91,4 @@ int main(int argc, char **argv)
     ROS_INFO("Image Decompression Node");
     return EXIT_SUCCESS;
 }
+
